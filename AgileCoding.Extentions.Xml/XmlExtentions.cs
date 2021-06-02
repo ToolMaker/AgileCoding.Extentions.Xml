bc0be721c867f68c7edae2ec53b39a77bb22018a
Ernie <gunning.ernie@gmail.com>
2021-06-02 13:42:03 +0200

    Project Start

---
+++ AgileCoding.Extentions.Xml/XmlExtentions.cs
@@ -0,0 +1,18 @@
+﻿namespace AgileCoding.Extentions.Xml
+{
+    public static class XmlExtentions
+    {
+        public static string StripNamespacePrefix(this string self)
+        {
+            int indexOfColon = self.IndexOf(':');
+
+            if (indexOfColon >= 0)
+            {
+                indexOfColon++;
+                return self.Substring(indexOfColon, self.Length - indexOfColon);
+            }
+
+            return self;
+        }
+    }
+}
---
+++ ReadMePage.md
@@ -0,0 +1,5 @@
+﻿#AgileCoding.Extentions.Xml ReadMePage
+
+###Extensions related to XML
+
+1.	XmlExtentions - Static class containing XML extentions
